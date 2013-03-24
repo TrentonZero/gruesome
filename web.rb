5a00cc076f24b769050f2c2aac8f9289f4034021
TrentonZero <trentonzero@gmail.com>
2013-03-23 19:23:52 -0500

    adding mongodb

--- web.rb
+++ web.rb
@@ -1,7 +1,30 @@
 require 'sinatra'
+require 'mongo' 
+require 'uri'
 
 get '/' do
-  "Hello, world. This is KJW."
+  puts "<p>Hello, world. This is KJW.</p>"
+  puts "<form name=\"input\" action=\"/\" method=\"post\">"
+  puts "<input type=\"submit\" value=\"Submit\">"
+  puts "</form>"
 end
 
+post '/' do
+  db = get_connection
+  
+  puts "Collections"
+  puts "==========="
+  collections = db.collection_names
+  puts collections
+end
 
+def get_connection
+  return @dbconnection if @db_connection
+  
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//,'')
+  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+  @db_connection
+  
+end
